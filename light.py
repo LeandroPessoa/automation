4d44254a43ad5581923841ff9bdd0748f0d07220
Leandro Pessoa <leandroeliasster@gmail.com>
2017-09-28 13:40:39 -0300

    commit inicial

---
+++ light.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO            # import RPi.GPIO module  
+from time import sleep             # lets us have a delay  
+GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
+GPIO.setup(18, GPIO.OUT)           # set GPIO24 as an output 
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/liga')
+def liga():
+    GPIO.output(18, 1)
+    return "Liga"
+
+@app.route('/desliga')
+def desliga():
+    GPIO.output(18, 0)
+    return "Desliga"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', threaded=True)
