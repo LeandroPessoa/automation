f15eeb91f5296ee4ee9d336797ad3813460ff29c
Leandro Pessoa <leandroeliasster@gmail.com>
2017-10-01 13:12:46 -0300

    correcao

--- light.py
+++ light.py
@@ -2,18 +2,18 @@ import RPi.GPIO as GPIO            # import RPi.GPIO module
 from time import sleep             # lets us have a delay  
 GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
 GPIO.setup(18, GPIO.OUT)           # set GPIO24 as an output 
-GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/pcliga')
 def ligapc():
-    GPIO.output(23, 0)
+    GPIO.output(24, 0)
     return "Liga"
 
 @app.route('/pcdesliga')
 def desligapc():
-    GPIO.output(23, 1)
+    GPIO.output(24, 1)
     return "Desliga"
 
 @app.route('/liga')
