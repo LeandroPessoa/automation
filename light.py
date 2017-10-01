c6ba07198d68b092332f1e693b7d6c7c05620dc3
Leandro Pessoa <leandroeliasster@gmail.com>
2017-10-01 13:01:11 -0300

    adicionado um relé

--- light.py
+++ light.py
@@ -2,9 +2,19 @@ import RPi.GPIO as GPIO            # import RPi.GPIO module
 from time import sleep             # lets us have a delay  
 GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
 GPIO.setup(18, GPIO.OUT)           # set GPIO24 as an output 
+GPIO.setup(23, GPIO.OUT)
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/pcliga')
+def liga():
+    GPIO.output(23, 0)
+    return "Liga
+
+@app.route('/pcdesliga')
+def liga():
+    GPIO.output(23, 1)
+    return "Desliga"
 
 @app.route('/liga')
 def liga():
