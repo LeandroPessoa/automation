0efe68d026fff8864d1a068a81e1c511ce645c7d
Leandro Elias <leandro.elias@vianetbrasil.com.br>
2018-09-24 23:21:17 -0300

    mudancas

--- light.py
+++ light.py
@@ -13,18 +13,6 @@ import netifaces as nif
 app = Flask(__name__)
 
 
-def mac_for_ip(ip):
-    'Returns a list of MACs for interfaces that have given IP, returns None if not found'
-    for i in nif.interfaces():
-        addrs = nif.ifaddresses(i)
-        try:
-            if_mac = addrs[nif.AF_LINK][0]['addr']
-            if_ip = addrs[nif.AF_INET][0]['addr']
-        except IndexError: #ignore ifaces that dont have MAC or IP
-            if_mac = if_ip = None
-        if if_ip == ip:
-            return if_mac
-    return None
 
 
 @app.route('/portaosocial')
@@ -37,7 +25,7 @@ def portaosocial():
 @app.route('/portaogaragem')
 def portaogaragem():
     #GPIO.output(27, 0)
-    print(mac_for_ip(request.remote_addr))
+    print(request.remote_addr)
 
     sleep(1)
     #GPIO.output(27, 1)
