dda148f9d38dfa1dc5abad2dc901b0524a09efdd
Leandro Elias <leandro.elias@vianetbrasil.com.br>
2018-09-24 23:10:23 -0300

    mudancas

--- light.py
+++ light.py
@@ -12,6 +12,21 @@ import netifaces as nif
 
 app = Flask(__name__)
 
+
+def mac_for_ip(ip):
+    'Returns a list of MACs for interfaces that have given IP, returns None if not found'
+    for i in nif.interfaces():
+        addrs = nif.ifaddresses(i)
+        try:
+            if_mac = addrs[nif.AF_LINK][0]['addr']
+            if_ip = addrs[nif.AF_INET][0]['addr']
+        except IndexError: #ignore ifaces that dont have MAC or IP
+            if_mac = if_ip = None
+        if if_ip == ip:
+            return if_mac
+    return None
+
+
 @app.route('/portaosocial')
 def portaosocial():
     GPIO.output(21, 0)
@@ -41,15 +56,3 @@ def hello():
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', threaded=True)
 
-def mac_for_ip(ip):
-    'Returns a list of MACs for interfaces that have given IP, returns None if not found'
-    for i in nif.interfaces():
-        addrs = nif.ifaddresses(i)
-        try:
-            if_mac = addrs[nif.AF_LINK][0]['addr']
-            if_ip = addrs[nif.AF_INET][0]['addr']
-        except IndexError: #ignore ifaces that dont have MAC or IP
-            if_mac = if_ip = None
-        if if_ip == ip:
-            return if_mac
-    return None
